08a641abf93face7ea8f109a9c535692cedaac73
Miguel Banuelos <mistergengar@gmail.com>
2014-11-13 16:02:20 -0800

    jk

---
+++ public_html/js/entities/entities.js
@@ -0,0 +1,12 @@
+// TODO
+game.playerEntinty = me.Entity.extend({
+  inti: function(x, y, settings){
+      
+ },
+ 
+ update: function(){
+     
+ }
+    
+    
+});
---
+++ public_html/js/resources.js
@@ -0,0 +1,30 @@
+game.resources = [
+    /* Graphics. 
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
+    {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    {name: "Mario", type:"image", src: "data/img/player1.png"},
+    
+    /* Atlases 
+     * @example
+     * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+     */
+
+    /* Maps. 
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
+    {name: "GengarL4", type: "tmx", src: "data/map/GengarL4.tmx"}
+
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
+
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
+];
